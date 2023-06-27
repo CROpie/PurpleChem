b972ad867c3c365cc042d5523562e5852cbd648b
CROpie <96667010+CROpie@users.noreply.github.com>
2023-06-27 10:46:54 +1000

    Added ability to order a chemical.

---
+++ src/routes/orderChemical/+page.server.ts
@@ -0,0 +1,69 @@
+import { fail } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
+import type { RequestEvent } from '../$types';
+
+export const actions: Actions = {
+	// orderChemical: async ({ request, locals: { supabase, getSession } }) => {
+	orderChemical: async (event: RequestEvent) => {
+		const formData = await event.request.formData();
+
+		if (!formData) {
+			return fail(400, { formData, missing: true });
+		}
+
+		const session = await event.locals.getSession();
+		const userID = session?.user.id;
+		const CAS = formData.get('CAS');
+		const chemicalName = formData.get('chemicalName');
+		const amount = formData.get('amount');
+		const amountUnit = formData.get('amountUnit');
+
+		// see if the chemical is in the database already, return object contains the ID if it exists
+		// need to use .maybeSingle() to return either one object or null
+		// without, it's an array or null
+		let chemical = await event.locals.supabase
+			.from('chemical')
+			.select()
+			.eq('CAS', CAS)
+			.maybeSingle();
+
+		// adding .select() to the end retuns the newly created data
+		if (!chemical.data) {
+			console.log('Adding new chemical to database...');
+			chemical = await event.locals.supabase
+				.from('chemical')
+				.insert({
+					CAS,
+					chemicalName
+				})
+				.select();
+		}
+
+		// add order to database
+		const { error } = await event.locals.supabase.from('order').insert({
+			userID,
+			chemicalID: chemical.data.id,
+			amount,
+			amountUnit
+		});
+
+		if (error) {
+			console.log('error');
+			return fail(400);
+		}
+		console.log('success');
+		return { success: true };
+
+		//return;
+	}
+};
+
+// import type { PageServerLoad } from './$types'
+// export const load: PageServerLoad = async ...
+
+// https://kit.svelte.dev/docs/types#public-types-requestevent
+// import type { RequestEvent } from '../$types';
+// orderChemical: async (event: RequestEvent) => {} // seems ok but an error on orderChemical for some reason ???
+// still works fine though
+
+// PostgrestSingleResponse (return object of accessing the DB)
--- src/routes/orderChemical/+page.svelte
+++ src/routes/orderChemical/+page.svelte
@@ -1,1 +1,65 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { SubmitFunction } from '@sveltejs/kit';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+
+	let CAS = '';
+	let chemicalName = '';
+	let notFound = false;
+
+	const getProperties = async () => {
+		console.log('searching...');
+		let uri = `https://commonchemistry.cas.org/api/detail?cas_rn=${CAS}`;
+		const res = await fetch(uri);
+		if (!res.ok) {
+			notFound = true;
+			return;
+		}
+		const data = await res.json();
+		chemicalName = data.name;
+	};
+
+	const orderChemical: SubmitFunction = async (event) => {
+		// do stg
+	};
+</script>
+
 <h1>Order Chemical</h1>
+<form method="POST" action="?/orderChemical" use:enhance={orderChemical}>
+	<div class="label-input-container">
+		<label for="CAS"
+			>CAS Number
+			<input id="CAS" name="CAS" type="text" bind:value={CAS} required />
+		</label>
+		<button type="button" on:click={() => getProperties()}>FIND</button>
+		{#if notFound}
+			<p>No information from this CAS number was obtained.</p>
+		{/if}
+		<label for="chemicalName">Chemical Name</label>
+		<input type="text" name="chemicalName" bind:value={chemicalName} />
+	</div>
+
+	<div class="label-input-container">
+		<label for="amount">Amount</label>
+		<input id="amount" name="amount" type="number" required />
+	</div>
+
+	<div class="label-input-container">
+		<label for="amountUnit">Unit</label>
+		<select id="amountUnit" name="amountUnit">
+			<option value="g">mg</option>
+			<option value="g">g</option>
+			<option value="ml">mL</option>
+			<option value="ml">L</option>
+		</select>
+	</div>
+
+	<div class="label-input-container">
+		<button type="submit">ORDER CHEMICAL</button>
+	</div>
+	{#if form?.success}
+		<p>Order successful.</p>
+	{/if}
+</form>
